91feebc59ee43ed98957c4ea6f87b8d793b15a6c
zhiangzz <zhiangz@umich.com>
2022-12-29 15:36:04 -0500

    update 1

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import CategoryItem from "../category-item/category-item.component";
+import './directory.styles.scss'
+const Directory = ({categories}) =>{
+    return (
+    <div className="directory-container">
+      {categories.map(({title, id,imageUrl})=>(
+        <CategoryItem
+          key={id}
+          title={title}
+          imageUrl={imageUrl}/>
+      ))}
+    </div>
+    )
+}
+export default  Directory;
